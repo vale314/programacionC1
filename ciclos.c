5121667f8af8b7de727d7c522591b677988bc70c
vale314 <electrovale314@hotmail.com>
2018-02-10 13:55:27 -0600

    ciclos y factorial

---
+++ ciclos.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+
+main(){
+    int num, i, max=0;
+    char rep, opC;
+
+   do{
+    printf("\t\n");
+    printf("1 para ciclos de for\n f\n F\n");
+    printf("2 para ciclos de while\n w\n W\n");
+    printf("3 para ciclos de do while\n d\n D\n");
+    fflush(stdin);
+    scanf("%c",&opC);
+
+    system("cls");
+
+    printf("Por favor introdusca el numero a repetir\n");
+    fflush(stdin);
+    scanf("%d",&num);
+
+    i=num; max=0;
+    switch(opC){
+        case '1': case 'f': case 'F':
+            for(i;i>=0;i--){
+                    if(i%2==0){
+                        printf(" el numero es %d\n",i);
+                        max=i+max;
+                    }
+
+
+            }
+            printf("La suma es: %d\nLa suma fue hecha con for\n", max);
+            system("pause");
+           break;
+
+        case '2': case 'w': case 'W':
+            while(i>0){
+                 if(i%2==0){
+                        printf(" el numero es %d\nLa suma fue hecha con while\n",i);
+                        max=i+max;
+                    }
+
+                i--;
+            }
+            printf("La suma es: %d\n", max);
+            system("pause");
+
+           break;
+
+        case '3': case 'd': case 'D':
+            do{
+                if(i%2==0){
+                        printf(" el numero es %d\nLa suma fue hecha con do\n",i);
+                        max=i+max;
+                    }
+
+                i--;
+            }while(i>0);
+            printf("La suma es: %d\n", max);
+            system("pause");
+            break;
+
+        default:
+            printf("Incorrecto Ninguna Opcion Del Menu \n");
+            system("pause");
+            break;
+
+    }
+    i=0;
+    system("cls");
+    printf("presione Y para salir y N Para proseguir\n");
+    fflush(stdin);
+    scanf("%c",&rep);
+    system("cls");
+    }while(rep == 'N' || rep == 'n');
+
+}
---
+++ factorial.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+
+main(){
+    int num, i;
+    long long max=1;
+    char rep, opC;
+
+   do{
+    printf("\t\n");
+    printf("1 para ciclos de for\n f\n F\n");
+    printf("2 para ciclos de while\n w\n W\n");
+    printf("3 para ciclos de do while\n d\n D\n");
+    fflush(stdin);
+    scanf("%c",&opC);
+
+    system("cls");
+
+    printf("Por favor introdusca el numero\n");
+    fflush(stdin);
+    scanf("%d",&num);
+    system("cls");
+    i=num; max=1;
+    switch(opC){
+        case '1': case 'f': case 'F':
+            printf("La Operacion Factorial fue hecha con For\n");
+            for(i;i>0;i--){
+                printf(" el numero es %d\n",i);
+                max=i*max;
+            }
+            printf("La Operacion Factorial es: %d\n", max);
+            system("pause");
+           break;
+
+        case '2': case 'w': case 'W':
+            printf("La Operacion Factorial fue hecha con While\n");
+            while(i>0){
+                printf(" el numero es %d\n",i);
+                max=i*max;
+
+                i--;
+            }
+           printf("La Operacion Factorial es: %d\n", max);
+           system("pause");
+
+           break;
+
+        case '3': case 'd': case 'D':
+            printf("La Operacion Factorial fue hecha con Do-While\n");
+            do{
+                printf(" el numero es %d\n",i);
+                max=i*max;
+
+                i--;
+            }while(i>0);
+            printf("La Operacion Factorial es: %d\n", max);
+            system("pause");
+            break;
+
+        default:
+            printf("Incorrecto Ninguna Opcion Del Menu \n");
+            system("pause");
+            break;
+
+    }
+    i=0;
+    system("cls");
+    printf("\npresione Y para salir y N Para proseguir\n");
+    fflush(stdin);
+    scanf("%c",&rep);
+    system("cls");
+    }while(rep == 'N' || rep == 'n');
+
+}
